0c68cc1cdc155a2f7d36712f20827f0513fc6d41
Manav Khadka <manavkhadka0@gmail.com>
2024-06-10 15:42:09 +0545

    only frontned now

--- backend/npf/siteconfigs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SiteconfigsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'siteconfigs'
--- backend/npf/siteconfigs/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import DonationContent
-from rest_framework import serializers
-
-
-class DonationContentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DonationContent
-        fields = "__all__"
