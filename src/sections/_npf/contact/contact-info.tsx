4479f78b62819b988016c479e0e13c2282b38ceb
Manav Khadka <manavkhadka0@gmail.com>
2024-06-23 13:03:53 +0545

    Refactor Google Map iframe width to be responsive

--- src/sections/_npf/contact/contact-info.tsx
+++ src/sections/_npf/contact/contact-info.tsx
@@ -94,8 +94,8 @@ export default function ContactInfo() {
         <Grid xs={12} md={6} lg={7}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2215.4964325176875!2d84.45110280584464!3d27.663164682953102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDM5JzQ4LjEiTiA4NMKwMjcnMDcuOSJF!5e1!3m2!1sen!2snp!4v1719031083658!5m2!1sen!2snp"
-            width="600"
             height="450"
+            width="100%"
             style={{ border: 0 }}
             allowFullScreen
             title="Google Map"
