b20b9ed8b9d1c0bf810b8c3939e5eb308379abf8
Manav Khadka <manavkhadka0@gmail.com>
2024-09-21 19:56:21 +0545

    Update next-env.d.ts to include correct link for TypeScript information

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,4 +1,4 @@
-import { Barlow, Public_Sans } from 'next/font/google';
+import { Libre_Franklin } from 'next/font/google';
 
 // ----------------------------------------------------------------------
 
@@ -31,14 +31,14 @@ declare module '@mui/material/styles' {
   }
 }
 
-export const primaryFont = Public_Sans({
+export const primaryFont = Libre_Franklin({
   weight: ['400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   display: 'swap',
   fallback: ['Helvetica', 'Arial', 'sans-serif'],
 });
 
-export const secondaryFont = Barlow({
+export const secondaryFont = Libre_Franklin({
   weight: ['400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   display: 'swap',
